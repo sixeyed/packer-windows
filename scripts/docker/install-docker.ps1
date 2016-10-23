5c0c2820c7470d45bc91ac383467628f66e317f7
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-23 11:28:46 +0200

    Add more info, add fix for docker/docker#27544

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,5 +1,16 @@
+Get-PackageSource
+Write-Host "Install-PackageProvider ..."
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Get-PackageSource
+Write-Host "Install-Module ..."
 Install-Module -Name DockerMsftProvider -Force
+Get-PackageSource
+Write-Host "Set-PSRepository trusted ..."
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+Write-Host "Install-Package ..."
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+Write-Host "Set-PSRepository untrusted ..."
 Set-PSRepository -InstallationPolicy Untrusted -Name PSGallery
+Write-Host "Fix --restart=always for reboot ..."
+# see https://github.com/docker/docker/issues/27544
+& sc config Docker depend=LanmanWorkstation
